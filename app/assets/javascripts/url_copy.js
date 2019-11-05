16d0c4a7728df7e5174c9d73e701bb979f4c8802
moriokashingo <uma20056@yahoo.co.jp>
2019-11-05 22:15:27 +0900

    Create url_copy.js
    cospul詳細ページのurlをコピーするボタンのjs

---
+++ app/assets/javascripts/url_copy.js
@@ -0,0 +1,7 @@
+function copyToClipboard() {
+  $('body').append('<textarea id="currentURL" style="position:fixed;left:-100%;">'+location.href+'</textarea>');
+  $('#currentURL').select();
+  document.execCommand('copy');
+  $('#currentURL').remove();
+  alert("URLをコピーしました。");
+}
