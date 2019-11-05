0e69c3d6abf7da6106f692884053288f13c80fe2
moriokashingo <uma20056@yahoo.co.jp>
2019-11-05 22:28:33 +0900

    Create intro_controller.rb
    チャレンジしたいもの用のコントローラー

---
+++ app/controllers/intro_controller.rb
@@ -0,0 +1,17 @@
+class IntroController < ApplicationController
+  def show
+  end
+
+  def tetris
+  end
+
+  def omikuji
+    @cospul_pictures =CospulPicture.all
+  end
+
+  def kaonashi
+  end
+
+  def test
+  end
+end
