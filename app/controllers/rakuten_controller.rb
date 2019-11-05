a80bd055e46c21f104d317607ad04e6d8939e7ec
moriokashingo <uma20056@yahoo.co.jp>
2019-11-05 22:28:52 +0900

    Create rakuten_controller.rb

---
+++ app/controllers/rakuten_controller.rb
@@ -0,0 +1,11 @@
+class RakutenController < ApplicationController
+  def index
+    RakutenWebService.configure do |c|
+      c.application_id = Rails.application.credentials.rakuten[:application_id]
+      c.affiliate_id = Rails.application.credentials.rakuten[:affiliate_id]
+    end
+    if params[:keyword]
+    @items = RakutenWebService::Ichiba::Item.search(keyword: params[:keyword])
+    end
+  end
+end
