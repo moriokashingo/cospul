a81d174b77a1be413850677a9e27cea020e90510
moriokashingo <uma20056@yahoo.co.jp>
2019-11-05 22:51:17 +0900

    Create rakuten.rb

---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,4 @@
+RakutenWebService.configure do |c|
+  c.application_id = Rails.application.credentials.rakuten[:application_id]
+  c.affiliate_id 　= Rails.application.credentials.rakuten[:affiliate_id]
+  end
