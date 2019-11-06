94b9a163d886a021a8907f45944f4f8f65aa6326
moriokashingo <uma20056@yahoo.co.jp>
2019-11-06 13:41:36 +0900

    create s3
    s3の導入

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
-# if Rails.env.production?
-#   CarrierWave.configure do |config|
-#     config.storage = :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: ENV['ACCESS_KEY_ID'],
-#       aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-#       region: 'ap-northeast-1' #例 'ap-northeast-1'
-#     }
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+      region: 'ap-northeast-1'
+    }
 
-#     config.fog_directory  = 'cospul2'
-#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cospul2'
-#   end
-# end
+    config.fog_directory  = 'cospul'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/cospul'
+  end
+end
