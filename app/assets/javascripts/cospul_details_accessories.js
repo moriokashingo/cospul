cfdb765166331d048d1a85472ba2ceb49e8b6d74
moriokashingo <uma20056@yahoo.co.jp>
2019-11-14 13:24:12 +0900

    Update cospul_details_accessories.js
    アクセサリー登録画面で元々あったアクセサリーを削除できるように変更

--- app/assets/javascripts/cospul_details_accessories.js
+++ app/assets/javascripts/cospul_details_accessories.js
@@ -1,25 +1,51 @@
 
 $(function() {
   var count = 0;
-  var accessories_list = $("#accessories-add");
+  var accessories_list = $(".new__accessories__box");
 
-    function appendList() {
-      var html =  ` <input placeholder="アクセサリーやその他の情報を入力してください。
-                    "name="cospul_detail[accessories_attributes][${ count }][name]"
-                    id="cospul_detail_accessories_attributes_${ count }_name",class="mt30">
-                    <div class='tag-search-remove chat-cospul-tag__btn chat-cospul-tag__btn--remove js-remove-btn',id="delete">削除</div>`
-      count = count + 1
-      accessories_list.append(html)
+  $(window).on("load", function(){
+    if(document.URL.match(/edit/) && document.URL.match(/cospul_details/)) {
+      count = $(".old__accessories").length
     }
-
-
-    $(accessories_list).on("click", '.js-remove-btn', function() {
-      $(this).prev().remove();
-      $(this).remove();
-    });
-
-
-    $("#accessories-add p").on("click", function() {
-      appendList()
-    });
   });
+
+  function inputDisplay(count) {
+    if (count < 5){
+      $("#accessories__add__btn").css({'display':'block',});
+    }else{
+      $("#accessories__add__btn").css({'display':'none',});
+    }
+  }
+
+  function appendList() {
+    var html =  ` <div class='new__accessories'>
+                    <input placeholder="アクセサリーやその他の情報を入力してください。"name="cospul_detail[accessories_attributes][${ count }][name]"id="cospul_detail_accessories_attributes_${ count }_name",class="mt30">
+                    <div class='new__accessories__remove-btn'>削除</div>
+                  </div>`
+    count += 1
+    accessories_list.append(html)
+    inputDisplay(count)
+  }
+
+  $(document).on("click", '.old__accessories__remove-btn', function() {
+    id = $(this).data("id")
+    var delete_accessories = $(this).parent().parent().find(`#cospul_detail_accessories_attributes_${id}__destroy`)
+    delete_accessories.prop('value', "1");
+    $(this).parent().remove();
+    $(this).remove();
+    count -= 1
+    inputDisplay(count)
+  });
+  $(document).on("click", '.new__accessories__remove-btn', function() {
+    $(this).parent().remove();
+    $(this).remove();
+    count -= 1
+    inputDisplay(count)
+  });
+
+
+  $("#accessories__add__btn").on("click", function() {
+    appendList()
+  });
+
+});
