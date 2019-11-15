9e01255d03a0bebddde5dcb36d3e65169c1fa09d
moriokashingo <uma20056@yahoo.co.jp>
2019-11-15 10:57:56 +0900

    delete Tag_model
    Tag,TagCospulモデルの削除

--- app/models/tag.rb
+++
@@ -1,6 +0,0 @@
-class Tag < ApplicationRecord
-  has_many  :tag_cospuls        ,dependent: :destroy
-  has_many  :cospuls            , through: :tag_cospuls,dependent: :destroy
-
-  validates :name               , uniqueness: { scope: :name }
-end
