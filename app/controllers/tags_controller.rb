0f09c9d5d6db94c560127d3468f76a0907f32b43
moriokashingo <uma20056@yahoo.co.jp>
2019-11-15 13:21:49 +0900

    create pagenation
    タグ検索、タグ一覧ページでのページネーションの追加（かなり助長になってしまったので改善の余地あり）

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,13 +1,19 @@
 class TagsController < ApplicationController
 
   def show
-    @cospuls = []
     tag = GutentagTag.find(params[:id])
-    chukan = GutentagTagging.where(tag_id: tag.id)
-    chukan.each do |tag|
+    @pre_cospuls = []
+    @cospul_ids = []
+    gutentag_tagging = GutentagTagging.where(tag_id: tag.id)
+    gutentag_tagging.each do |tag|
       @one_cospul = Cospul.find(tag.taggable_id)
-      @cospuls << @one_cospul
+      @pre_cospuls << @one_cospul
     end
+    @pre_cospuls.each do |cospul|
+      @one_cospul_id= cospul.id
+      @cospul_ids << @one_cospul_id
+    end
+    @cospuls =Cospul.where(id: @cospul_ids).order("created_at DESC").page(params[:page]).per(9)
     @name = tag.name
   end
 
