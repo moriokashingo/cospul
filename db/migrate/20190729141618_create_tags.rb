f698d5ea39e83076f238b68856b70e3dc5c877e5
moriokashingo <uma20056@yahoo.co.jp>
2019-11-15 11:00:43 +0900

    change tag's_tables
    gem"gutentag"ようにテーブルの削除と追加

--- db/migrate/20190729141618_create_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateTags < ActiveRecord::Migration[5.2]
-  def change
-    create_table :tags do |t|
-      t.string :name  , null: false
-      t.index  :name  , unique: true
-      t.timestamps
-    end
-  end
-end
