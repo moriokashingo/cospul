ba55c7de59c8c6a693c51badc186aa1ec6546358
moriokashingo <uma20056@yahoo.co.jp>
2019-11-16 18:05:40 +0900

    Update url_copy.js
    function包み直し

--- app/assets/javascripts/url_copy.js
+++ app/assets/javascripts/url_copy.js
@@ -1,7 +1,9 @@
-function copyToClipboard() {
-  $('body').append('<textarea id="currentURL" style="position:fixed;left:-100%;">'+location.href+'</textarea>');
-  $('#currentURL').select();
-  document.execCommand('copy');
-  $('#currentURL').remove();
-  alert("URLをコピーしました。");
-}
+$(function () {
+  function copyToClipboard() {
+    $('body').append('<textarea id="currentURL" style="position:fixed;left:-100%;">'+location.href+'</textarea>');
+    $('#currentURL').select();
+    document.execCommand('copy');
+    $('#currentURL').remove();
+    alert("URLをコピーしました。");
+  }
+});
