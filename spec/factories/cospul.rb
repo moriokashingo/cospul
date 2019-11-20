0546dd14a1a7327fd5a36f17bcde9ce3f80e1bfc
moriokashingo <uma20056@yahoo.co.jp>
2019-11-20 14:50:20 +0900

    create_cospul_no_picture
    画像のないcospulの追加とテストの記述

--- spec/factories/cospul.rb
+++ spec/factories/cospul.rb
@@ -4,6 +4,17 @@ FactoryBot.define do
     title            { "テストタイトル" }
     content          { "テスト詳細" }
     association      :user, factory: :user
+    after(:build) do |cospul|
+      cospul.cospul_pictures<< build(:cospul_picture, cospul: cospul)
+    end
+    after(:create) do |cospul|
+      # create(:gutentag_taggings, cospul: cospul, gutentag_tag: create(gutentag_tag))
+      create_list(:gutentag_tag, 3, cospuls: [cospul])
+    end
   end
-
+  factory :cospul_no_picture, class: Cospul do
+    title            { "テストタイトル" }
+    content          { "テスト詳細" }
+    association      :user, factory: :user
+  end
 end
--- spec/models/cospul_spec.rb
+++ spec/models/cospul_spec.rb
@@ -3,25 +3,30 @@ require 'rails_helper'
 RSpec.describe Cospul, type: :model do
 
   before do
-    @user           = FactoryBot.build(:user)
-    @cospul         = FactoryBot.build(:cospul)
-    @cospul_picture = FactoryBot.build(:cospul_picture)
+    @user                      = FactoryBot.build(:user)
+    @cospul                    = FactoryBot.build(:cospul)
+    @cospul_picture            = FactoryBot.build(:cospul_picture)
+    @cospul_no_picture         = FactoryBot.build(:cospul_no_picture)
+
   end
 
   it "has a valid factory of user" do
     user = @user
     expect(user).to be_valid
   end
-# # バリデーション
-#   it "is valid with a :name,:price,:region,:delivery_fee,:shipping_method,:delivery_days,:brand,:category,:user" do
-#     cospul = @cospul
-#     expect(cospul).to be_valid
-#   end
-it "is valid with a :name,:price,:region,:delivery_fee,:shipping_method,:delivery_days,:brand,:category,:user" do
-  cospul = @cospul
-  expect(cospul).to be_valid
-end
 
+  it "is valid with a :title,:content,:user,:cospul_picture" do
+    cospul = @cospul
+    expect(cospul).to be_valid
+  end
+
+
+
+  it "is invalid without a cospul_pictures" do
+    cospul_no_picture = @cospul_no_picture
+    cospul_no_picture.valid?
+    expect(cospul_no_picture.errors[:cospul_pictures]).to include("を入力してください")
+  end
 
 
   it "is invalid without a title" do
@@ -39,7 +44,7 @@ end
 it "is invalid with title more than 40 words" do
   cospul = build(:cospul, title: "a"* 41,)
   cospul.valid?
-  expect(cospul.errors[:title]).to include("は40文字以内で入力してください")  
+  expect(cospul.errors[:title]).to include("は40文字以内で入力してください")
 end
 it "is invalid with content more than 1000 words" do
   cospul = build(:cospul, content: "a" * 1001,)
